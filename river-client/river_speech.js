2fccbad58aa3a5d9cec25e1c6746774e816ad719
Brian <brianflaherty64@gmail.com>
2014-12-18 22:19:21 -0500

    edited voice for Festival

--- river-client/river_speech.js
+++ river-client/river_speech.js
@@ -10,32 +10,35 @@ var RiverSpeech = {
   test: function () {
     async.waterfall([
       function(cb) {
-        say.speak('Agnes', 'Agnes says... whats up, dog?', cb);
-      },
-      function(cb) {
-        say.speak(null, 'Default voice says... Hello!', cb);
-      },
-      function(cb) {
-        say.speak('Kathy', 'Kathy says, hows it goin?', cb);
-      },
-      function(cb) {
-        say.speak('Princess', 'Princess says, hi there   hehe?', cb);
-      },
-      function(cb) {
-        say.speak('Vicki', 'Vicki says, aloha friend?', cb);
-      },
-      function(cb) {
-        say.speak('Victoria', 'Victoria says, howdy partner', cb);
-      },
-      function(cb) {
-        say.speak('Zarvox', 'Zarvox says, howdy partner', cb);
-      },
-      function(cb) {
-        say.speak('Good News', 'Good news says, howdy partner', cb);
-      },
-      function(cb) {
-        say.speak('Whisper', 'Whisper says, howdy partner', cb);
-      },
+        say.speak('slt_artic_hts', 'Hello.  My name is River.', cb);
+      }
+      // function(cb) {
+      //   say.speak('Agnes', 'Agnes says... whats up, dog?', cb);
+      // },
+      // function(cb) {
+      //   say.speak(null, 'Default voice says... Hello!', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Kathy', 'Kathy says, hows it goin?', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Princess', 'Princess says, hi there   hehe?', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Vicki', 'Vicki says, aloha friend?', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Victoria', 'Victoria says, howdy partner', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Zarvox', 'Zarvox says, howdy partner', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Good News', 'Good news says, howdy partner', cb);
+      // },
+      // function(cb) {
+      //   say.speak('Whisper', 'Whisper says, howdy partner', cb);
+      // },
     ], null);
   }
 }
